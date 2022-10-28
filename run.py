be9d43a880b8fcd72dccc778126250af724eed35
Mike Rae <mikerae@me.com>
2022-10-28 10:49:09 +0000

    Add additional pages and routs

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-      
+
 
 @app.route("/about")
 def about():
@@ -18,11 +18,16 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-     
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
+        debug=True,
     )
